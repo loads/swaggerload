05dfb69b03eb300730ee239a6b47bd7cdf2bb88c
Tarek Ziade <tarek@ziade.org>
2016-10-25 10:40:13 +0200

    prototyping with shavar

--- parser.py
+++ parser.py
@@ -1,7 +1,12 @@
 from swagger_parser import SwaggerParser
 from pprint import pprint
 
-parser = SwaggerParser(swagger_path='api-with-examples.yaml')
+parser = SwaggerParser(swagger_path='shavar.yaml')
 
-pprint(parser.specification['info'])
 
+print('Endpoints:')
+print('')
+for path, spec in parser.specification['paths'].items():
+    print('%s %s' % (list(spec.keys())[0].upper(), path))
+
+#pprint(parser.specification['info'])
